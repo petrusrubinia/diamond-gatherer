bc3850780878f63cae132efc1b434e4cb8de677c
Stefania <petrusrubinia@yahoo.com>
2020-11-11 19:39:31 +0200

    Introducere html, css, javascript, canvas

---
+++ main.js
@@ -0,0 +1,17 @@
+var name = "Paul"; // string
+var amount = 23; // numeric
+var knowJavascript = true; //boolean
+var fruits = ["apple", "lemon", "orange"]; //Array
+
+//object
+var person = {
+    height: 2,
+    canWalk: function() {
+        console.log("Yes, I can walk.\n");
+    },
+    name: "Julia"
+};
+
+function add(){
+    return 2 + 3;
+}
